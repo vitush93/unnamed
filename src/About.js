87109beba8e866d2821792abec63a663849bdb8f
Vit Habada <vithabada93@gmail.com>
2017-09-06 02:06:55 +0200

    header fix

--- src/About.js
+++ src/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Grid} from "semantic-ui-react";
+import {Grid, Header} from "semantic-ui-react";
 
 const About = () => {
     return (
         <Grid columns={1}>
             <Grid.Row>
                 <Grid.Column>
-                    <h1>About Oculare</h1>
+                    <Header as='h1'>About Oculare</Header>
                     <p>Lorem ipsum dolor sit amet.</p>
                 </Grid.Column>
             </Grid.Row>
